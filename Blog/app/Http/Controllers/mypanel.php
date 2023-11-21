e60f7ed05790cf1a9da8d14c397f67c18bb3cffd
susmita1994 <susmitadhua2012@gmail.com>
2023-11-21 18:39:50 +0530

    Initial commit

---
+++ Blog/app/Http/Controllers/mypanel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class mypanel extends Controller
+{
+   function index() : View {
+        return view('mypanel.index');
+
+   }
+}
